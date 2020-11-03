def14cfd7a70128eaec955a268ab81813e74fc1e
Ania <anna.polgert@gmail.com>
2020-11-03 15:53:14 +0100

    First commit

---
+++ main.js
@@ -0,0 +1,26 @@
+// ------------------------PIERWSZA OPCJA------------------------------------
+
+//Get the button:
+scrollButton = document.getElementById("scroll-top-button");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 250 || document.documentElement.scrollTop > 250) {
+    scrollButton.style.display = "block";
+  } else {
+    scrollButton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
+
+//----------------------- DRUGA OPCJA --------------------------------
+
+// const scrollToTopBtn = document.getElementById("scroll-top-button")

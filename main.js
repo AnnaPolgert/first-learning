8cfa11b3f03662ef8037728479b7971c43d5d2a1
Ania <anna.polgert@gmail.com>
2020-11-09 00:52:56 +0100

    styled mode switcher

--- main.js
+++ main.js
@@ -6,10 +6,10 @@ const scrollButton = document.querySelector(".scroll-top-button");
 const showHideButton = function() {
   if (document.body.scrollTop > 250 || document.documentElement.scrollTop > 250) {
     // scrollButton.style.display = "block";
-    scrollButton.classList.add('scroll-top-button--active');
+    scrollButton.classList.add("scroll-top-button--active");
   } else {
     // scrollButton.style.display = "none";
-    scrollButton.classList.remove('scroll-top-button--active');
+    scrollButton.classList.remove("scroll-top-button--active");
   }
 }
 
@@ -33,14 +33,16 @@ window.addEventListener("scroll", showHideButton);
 
 // --------------------- Variables -------------------------
 
-const modeButton = document.querySelector(".mode-switch-button");
+const modeButton = document.querySelector(".mode-switcher");
 
 const switchMode = function() {
   const body = document.querySelector("body");
   if (body.classList.contains("light-mode")) {
     body.classList.remove("light-mode");
+    // TO DO: change text to dark
   } else {
     body.classList.add("light-mode");
+    // TO DO: change text to light
   }
 }
 
